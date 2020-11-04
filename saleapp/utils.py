4536b6dc20638bac93099333effc694957167a14
Thanh Duong <dhthanhqa@gmail.com>
2020-11-04 14:43:09 +0700

    chuc nang tim kiem

--- saleapp/utils.py
+++ saleapp/utils.py
@@ -6,6 +6,26 @@ def read_data(path='data/categories.json'):
         return json.load(f)
 
 
+def read_products(kw=None, cate_id=None,
+                  from_price=None, to_price=None):
+    products = read_data(path='data/products.json')
+
+    if cate_id:
+        cate_id = int(cate_id)
+        products = [p for p in products if (p['category_id'] == cate_id)]
+
+    if kw:
+        products = [p for p in products if p['name'].find(kw) >= 0]
+
+    if from_price and to_price:
+        from_price = float(from_price)
+        to_price = float(to_price)
+
+        products = [p for p in products if p['price'] >= from_price and p['price'] <= to_price]
+
+    return products
+
+
 def get_product_by_id(product_id):
     products = read_data('data/products.json')
     for p in products:
